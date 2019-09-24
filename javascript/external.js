49dc4fccdc686b1b4a2fe450b68f9191166c8925
Starke <rts56@txstate.edu>
2019-09-23 20:05:49 -0500

    Completed Task 5. Conditional Statement

--- javascript/external.js
+++ javascript/external.js
@@ -9,8 +9,21 @@
 //A prompt box is used to prompt users to input a value before entering a page.
 //user_name = window.prompt("Please enter your name", "Type your name here");
 //document.write(user_name);
-x = 22;
-const y = 33;
-document.writeln(x + y);
-document.writeln("<br>");
-document.writeln(x += 33);
+//x = 22;
+//const x = 33;
+//document.writeln(x + y);
+//document.writeln("<br>");
+//document.writeln(x += 33);
+document.writeln("<button onclick='condition()'>Conditional Test</button>")
+function condition()
+{
+  x = confirm("Are you sure you want to proceed?");
+  if(x)
+  {
+    document.writeln("You chose Okay!");
+  }
+  else
+   {
+    document.writeln("You chose cancel!");
+    }
+}
