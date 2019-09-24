95306da2602b81d2dde4260ed7b519e4616e8c7c
Robbie Starke <Robbie@Robbies-MacBook-Pro.local>
2019-09-23 20:29:47 -0500

    Saved users name

--- javascript/external.js
+++ javascript/external.js
@@ -7,13 +7,14 @@
 //const out = x + y;
 //document.write(out);
 //A prompt box is used to prompt users to input a value before entering a page.
-//user_name = window.prompt("Please enter your name", "Type your name here");
-//document.write(user_name);
+user_name = window.prompt("Please enter your name", "Type your name here");
+document.write(user_name);
 //x = 22;
 //const x = 33;
 //document.writeln(x + y);
 //document.writeln("<br>");
 //document.writeln(x += 33);
+
 document.writeln("<button onclick='condition()'>Conditional Test</button>")
 function condition()
 {
