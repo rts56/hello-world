794cfd73f2e6aab9b6e00c6ac4c9c106bea6af03
Robbie Starke <Robbie@Robbies-MBP.lan1>
2019-10-03 00:13:25 -0500

    Filled content from webmap array to webmap table

--- javascript/external.js
+++ javascript/external.js
@@ -1,3 +1,30 @@
+var webmaps =
+  [
+    ["Google Maps",
+    "https://www.google.com/maps/",
+    "Google Maps is probably the most well-known and used GPS/Mapping application in the US. The primary purpose of google maps is to provide GPS navigation to the public; although it has many other uses including street view & satellite imagery."
+  ],
+  ["Mapquest",
+  "https://www.mapquest.com/",
+  "Mapquest was the first commercial web mapping services available to users, launching in 1996. The advanced technology (at the time) was groundbreaking, making it the most popular web mapping services in the 2000s."
+  ]
+];
+
+function webmap_table()
+{
+  document.write("<table width=100%>");
+  for (var row=0; row < webmaps.length; row++)
+  {
+    document.write("<tr>");
+    for (var column=0; column < webmaps[0].length; column++)
+    {
+      document.write("<td>" + webmaps[row][column] + "</td>");
+    }
+    document.write("</tr>");
+  }
+  document.write("</table>");
+}
+
 function welcome()
 {
    let a = "Please enter your name.";
@@ -9,19 +36,4 @@ user_name = window.prompt(a,b);
 message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
 
 return message
-}
-
-function webmap_table()
-{
-  document.write("<table width=100%>");
-  for (var row=0; row < 2; row++)
-  {
-    document.write("<tr>");
-    for (var column=0; column < 3; column++)
-    {
-      document.write("<td>" + row + "," + column + "</td>");
-    }
-    document.write("</tr>");
-  }
-  document.write("</table>");
 }
