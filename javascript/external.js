babd011b81f22d9e8f91171e24d606ebc3d763ea
Robbie Starke <Robbie@Robbies-MacBook-Pro.local>
2019-10-07 16:12:59 -0500

    Improved Webmap Table aesthetics

--- javascript/external.js
+++ javascript/external.js
@@ -18,7 +18,13 @@ function webmap_table()
     document.write("<tr>");
     for (var column=0; column < webmaps[0].length; column++)
     {
-      document.write("<td>" + webmaps[row][column] + "</td>");
+      if(column<2){
+        document.write("<td>" + webmaps[row][column] + "</td>");
+      }
+      else {
+        document.write("</tr> <tr> <td colspan='2'>" + webmaps[row][column] + "<p></td></tr>");
+      }
+
     }
     document.write("</tr>");
   }
